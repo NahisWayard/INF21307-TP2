4e1b6edadea38ed89c574bbb04b81da459f52afe
Lilian Arago <lilian.arago@outlook.fr>
2021-03-12 01:01:50 +0100

    */ operators for MyFixed

--- src/MyFixed.hpp
+++ src/MyFixed.hpp
@@ -8,42 +8,65 @@ class MyFixed {
 public:
     explicit MyFixed(float input) {
         data = toFixed(input);
-    };
+    }
 
     explicit MyFixed(T input) {
         data = input;
-    };
+    }
 
-private:
     T data;
+private:
 
     static T toFixed(float f) {
         return f * (float)(1 << P);
-    };
+    }
+
     float toFloat() const {
         return (float)data / (float)(1 << P);
-    };
+    }
 
 public:
     friend MyFixed operator +(const MyFixed<T, P> &lhs, const MyFixed<T, P> &rhs) {
         return MyFixed<T, P>((T) (lhs.data + rhs.data));
-    };
+    }
+
     friend MyFixed operator -(const MyFixed<T, P> &lhs, const MyFixed<T, P> &rhs) {
         return MyFixed<T, P>((T) (lhs.data - rhs.data));
-    };
-    //friend MyFixed operator *(const MyFixed<T, P> &lhs, const MyFixed<T, P> &rhs);
-    //friend MyFixed operator /(const MyFixed<T, P> &lhs, const MyFixed<T, P> &rhs);
+    }
+
+    friend MyFixed operator *(const MyFixed<T, P> &lhs, const MyFixed<T, P> &rhs) {
+        size_t offset = (P / 2);
+        T a = (lhs.data >> offset);
+        T b = (rhs.data >> offset);
+        T r = (a * b) >> (P - (offset * 2));
+
+        return MyFixed<T, P>(r);
+    }
+
+    friend MyFixed operator /(const MyFixed<T, P> &lhs, const MyFixed<T, P> &rhs) {
+        size_t offset = (P / 2);
+        T a = (lhs.data >> offset);
+        T b = (rhs.data >> offset);
+        T r = (a / b) >> (P - (offset * 2));
+
+        return MyFixed<T, P>(r);
+    }
 
     friend bool operator ==(const MyFixed &lhs, const MyFixed &rhs) {
         return lhs.data == rhs.data;
-    };
+    }
+
     friend bool operator !=(const MyFixed &lhs, const MyFixed &rhs) {
         return !(rhs == lhs);
-    };
+    }
 
     friend std::ostream &operator <<(std::ostream &os, const MyFixed<T, P> &f) {
         os << f.toFloat();
         return os;
+    }
+
+    explicit operator float () const {
+        return toFloat();
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,20 @@
 #include <iostream>
-#include "MyFloat.hpp"
+#include <bitset>
 #include "MyFixed.hpp"
 
 int main(int ac, char **av) {
-    /* MY FIXED */
     typedef MyFixed<uint16_t, 4> FixedForTP;
 
-    auto f = FixedForTP(6.51f);
-    auto d = FixedForTP(2.125f);
+    auto f = FixedForTP(1.1f);
+    auto d = FixedForTP(1.25f);
 
-    auto res = f + d;
-    std::cout << res << std::endl;
-    res = f - d;
-    std::cout << res << std::endl;
-    /* END MY FIXED */
+    auto epsilon = FixedForTP(1.0f);
+    epsilon.data = 1;
 
-    //auto f = MyFloat(0b0000000001110011);
-    //auto f = MyFloat(124);
-    //auto f = MyFloat(6.51f);
-    //auto d = MyFloat(2.125f);
+    std::cout << epsilon << std::endl;
 
-    //auto res = f + d;
-
-    //auto res2 = f-d;
-    //auto t = MyFloat(34);
-
-    //uint16_t test = f.testUintToFix(124);
-    //uint16_t test = f.testFloatToFix(7.25f);
-
-    //f.testFixToFloat(test);
-    //uint16_t test2 = f.testDoubleToFix(7.9);
-
-    //f.testFixToDouble(test + test2);
-    // uint16_t testDoubleToFix(double x);
-    // double testFixToDouble(uint16_t x);
-    //f.testUintToFix(test);
-    // uint16_t testFixToInt(uint16_t x);
-
-    //f.testFixToInt(test);
-    //f.testFixToDouble(t);
-
-    //std::cout << f-t;
-
-    // f.setSignificand(0b1111);
-    // f.setExponent(0b1010);
-
-    // f.setExponent(f.getExponent() + 1);
-    // std::cout << f << std::endl;
-
-    // auto f1 = MyFloat(f.getSignificand(), f.getExponent());
-    // std::cout << f1 << std::endl;
+    auto res = f * d;
+    std::cout << f << "*" << d << "=" << res << std::endl;
+    res = f / d;
+    std::cout << f << "/" << d << "=" << res << std::endl;
 }
